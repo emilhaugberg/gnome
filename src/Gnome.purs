3c7df739f915e290b88e88f98a006fbb43224f31
Emil Haugberg <emilveen@gmail.com>
2017-09-30 15:31:30 +0200

    Fix gnome sorting.

--- src/Gnome.purs
+++ src/Gnome.purs
@@ -1,24 +1,26 @@
 module Gnome where
 
-import Prelude
-import Data.Array
-import Data.Maybe
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Prelude (class Ord, bind, const, join, pure, ($), (+), (-), (<), (<=), (==))
+import Data.Array (index, length, modifyAt)
+import Data.Maybe (Maybe)
 
 type Position = Int
 
 gnome :: forall a. (Ord a) => Position -> Position -> Array a -> Maybe (Array a)
-gnome x y arr = if x < length arr then swap else pure arr
+gnome x y arr = if x < length arr then sort else pure arr
   where
-    swap = join do
+    sort :: _
+    sort = join $ pure do
       previous <- index arr (x - 1)
       current  <- index arr x
 
       if previous <= current
-        then pure $ gnome y (y+1) arr
+        then gnome y (y + 1) arr
         else do
-          newArr1 <- modifyAt (x - 1) (\z -> current) arr
-          newArr2 <- modifyAt x (\z -> previous) newArr1
+          newArr1 <- swap (x - 1) (const current)  arr
+          newArr2 <- swap x       (const previous) newArr1
 
-          if x < 1 then pure $ gnome (y + 1) y newArr2 else pure $ gnome (x - 1) y newArr2
+          gnome (if x - 1 == 0 then y else (x - 1)) y newArr2
+
+    swap :: _
+    swap = modifyAt
--- test/Main.purs
+++ test/Main.purs
@@ -1,10 +1,11 @@
 module Test.Main where
 
-import Prelude
-import Gnome
+import Prelude (Unit, discard, show, ($))
+import Gnome (gnome)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   log $ show $ gnome 1 2 [1,2,3,5,4,3]
+  log $ show $ gnome 1 2 [6,5,4,3,2,1]
